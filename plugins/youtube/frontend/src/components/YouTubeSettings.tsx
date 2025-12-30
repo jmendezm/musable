1fb226cf58db7754868cbec218f13e5f34e5ae66
BreadJS <cuperus.kevin@hotmail.com>
2025-12-30 01:51:56 +0100

    Remove plugins, moved to different repo

--- plugins/youtube/frontend/src/components/YouTubeSettings.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const YouTubeSettings: React.FC = () => {
-  return React.createElement('div', { className: 'youtube-settings' },
-    React.createElement('h2', null, 'YouTube Plugin Settings'),
-    React.createElement('p', null, 'Configure YouTube integration settings')
-  );
-};
--- plugins/youtube/frontend/src/pages/YouTubeSearchPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const YouTubeSearchPage: React.FC = () => {
-  return React.createElement('div', { className: 'youtube-search-page' },
-    React.createElement('h1', null, 'YouTube Music Search'),
-    React.createElement('p', null, 'Search and download music from YouTube Music')
-  );
-};
